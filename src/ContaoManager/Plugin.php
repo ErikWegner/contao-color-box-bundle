15c9381b128ddcb8b7fbc599f5bb17c285ef8b51
Erik Wegner <E_Wegner@web.de>
2019-02-05 21:54:04 +0100

    Plugin namespace

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -8,7 +8,7 @@
  * @license LGPL-3.0-or-later
  */
 
-namespace Contao\ColorBoxBundle\ContaoManager;
+namespace ErikWegner\ColorBoxBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
